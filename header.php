5a41e60355697d380758db7d983c0ef4d6e922fd
Mizwa Adli <95085669+mizwa56@users.noreply.github.com>
2025-01-22 15:35:09 +0800

    testing 'test' branch. removing unnecessary JS and CSS files

--- header.php
+++ header.php
@@ -6,9 +6,11 @@
     ?>
     <title><?php echo $title ?> | CARISA</title>
 
-    <link rel="stylesheet" href="assets/dist/css/bootstrap.css">
+    <!-- Bootstrap v5.3 -->
     <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+    <!-- jQuery-I CSS -->
     <link rel="stylesheet" href="assets/dist/css/jquery-ui.min.css">
+    <!-- CARISA CSS -->
     <link rel="stylesheet" href="assets/style.css">
 
     <!-- Google Font -->
@@ -16,7 +18,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
+    <!-- jQuery and jQuery-ui JavaScript -->
     <script src="assets\dist\js\jquery.min.js"></script>
     <script src="assets\dist\js\jquery-ui.min.js"></script>
-    <!-- <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"> -->
 </head>
